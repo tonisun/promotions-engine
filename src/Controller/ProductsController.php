87a5fe8624943e98f82df0bd3b33a47bc82616a9
Toni Zeidler <toni.z@gmx.de>
2022-11-10 16:25:52 +0100

    new start

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,15 +2,64 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ProductsController extends AbstractController
-{
-    #[Route('/products', name: 'app_products')]
-    public function index(): JsonResponse
-    {
+use App\DTO\LowestPriceEnquiry;
+use App\Service\Serializer\DTOSerializer;
+
+class ProductsController extends AbstractController {
+
+    #[Route('/products/{id}/lowest-price', name: 'lowest-price', methods: ['GET'])]
+    public function lowestPrice ( Request $request, int $id, DTOSerializer $serializer ): Response{
+        //dd ( $id );
+        
+        if ( $request->headers->has('force_fail')) {
+            return new JsonResponse (
+                ['error' => 'Promotions Engine failure message',],
+                $request->headers->get('force_fail')
+            );
+        }
+
+        //dd($serializer);
+    
+        // 1. Deserialize json data into a EnquiryDTO
+        $lowestPriceEnquiry = $serializer->deserialize($request->getContent(), LowestPriceEnquiry::class, 'json');
+        
+        //dd($lowestPriceEnquiry);
+        
+        // 2. Pass the Enquiry (Anfrage) into a promotions filter
+            // the appropriate (entsprechende) promotion will be applied (angewendet)
+        
+
+
+        // 3. Return the modified Enquiry
+        $lowestPriceEnquiry->setDiscountedPrice(50);
+        $lowestPriceEnquiry->setPrice(100);
+        $lowestPriceEnquiry->setPromotionId(3);
+        $lowestPriceEnquiry->setPromotionName('Black Friday half price sale');
+        
+        $responseContent = $serializer->serialize($lowestPriceEnquiry, 'json' );
+        
+        return new Response($responseContent, 200);
+    }
+
+
+
+
+
+
+
+
+    #[Route('/products/{id}/promotions', name: 'promotions', methods: 'GET')]
+    public function promotions (): JsonResponse {
+
+
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/ProductsController.php',
---
+++ src/DTO/PromotionEnquiryInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\DTO;
+
+interface PromotionEnquiryInterface extends \JsonSerializable {
+    
+}
